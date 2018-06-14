4cbf1c157deab36f306fae2c3480351b44afecc0
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-06-14 14:21:21 -0400

    Create and persist Unit of Measure Class

---
+++ src/main/java/com/ubnik/spring5/recipes/recipeproject/domain/UnitOfMeasure.java
@@ -0,0 +1,34 @@
+package com.ubnik.spring5.recipes.recipeproject.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Joe Corapi on 6/14/2018.
+ */
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
