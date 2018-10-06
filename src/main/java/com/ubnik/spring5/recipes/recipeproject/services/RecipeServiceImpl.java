800a85a9334da5f04ca6bf26d1b8ec65733c2cfa
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-10-05 23:46:02 -0400

    Annotate Directions as @Lob and Service @Service

--- src/main/java/com/ubnik/spring5/recipes/recipeproject/services/RecipeServiceImpl.java
+++ src/main/java/com/ubnik/spring5/recipes/recipeproject/services/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package com.ubnik.spring5.recipes.recipeproject.services;
 
 import com.ubnik.spring5.recipes.recipeproject.domain.Recipe;
 import com.ubnik.spring5.recipes.recipeproject.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -9,6 +10,7 @@ import java.util.Set;
 /**
  * Created by Joe Corapi on 6/21/2018.
  */
+@Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
