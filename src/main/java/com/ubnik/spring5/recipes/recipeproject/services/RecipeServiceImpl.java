34a64e159118374a3dd27327d875aec0147dda28
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-10-10 16:57:56 -0400

    Add Slf4j logger to service

--- src/main/java/com/ubnik/spring5/recipes/recipeproject/services/RecipeServiceImpl.java
+++ src/main/java/com/ubnik/spring5/recipes/recipeproject/services/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package com.ubnik.spring5.recipes.recipeproject.services;
 
 import com.ubnik.spring5.recipes.recipeproject.domain.Recipe;
 import com.ubnik.spring5.recipes.recipeproject.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -10,6 +11,7 @@ import java.util.Set;
 /**
  * Created by Joe Corapi on 6/21/2018.
  */
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -21,6 +23,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm logging in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
