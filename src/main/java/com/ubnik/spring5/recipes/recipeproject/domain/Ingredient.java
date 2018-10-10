faf2b6bd16b17ce89d350627135ab785456ef039
Joe Corapi <24542019+JoeCorapi@users.noreply.github.com>
2018-10-10 17:07:54 -0400

    Refactor with Lombok @Data

--- src/main/java/com/ubnik/spring5/recipes/recipeproject/domain/Ingredient.java
+++ src/main/java/com/ubnik/spring5/recipes/recipeproject/domain/Ingredient.java
@@ -1,11 +1,14 @@
 package com.ubnik.spring5.recipes.recipeproject.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  * Created by Joe Corapi on 6/14/2018.
  */
+@Data
 @Entity
 public class Ingredient {
 
@@ -37,35 +40,4 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 }
--- src/main/java/com/ubnik/spring5/recipes/recipeproject/domain/UnitOfMeasure.java
+++ src/main/java/com/ubnik/spring5/recipes/recipeproject/domain/UnitOfMeasure.java
@@ -1,5 +1,7 @@
 package com.ubnik.spring5.recipes.recipeproject.domain;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +10,7 @@ import javax.persistence.Id;
 /**
  * Created by Joe Corapi on 6/14/2018.
  */
+@Data
 @Entity
 public class UnitOfMeasure {
 
@@ -16,19 +19,4 @@ public class UnitOfMeasure {
     private Long id;
     private String description;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
